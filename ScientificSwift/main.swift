e33285a4bf972202e3b0bb5a540f66c4f923fd35
stephenwstrom <strom@acm.org>
2017-04-17 13:47:58 -0400

    Initial Commit

---
+++ ScientificSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScientificSwift
+//
+//  Created by STEPHEN STROM on 4/17/17.
+//  Copyright © 2017 STEPHEN STROM. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
